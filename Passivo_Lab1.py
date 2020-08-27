17b2e7692c19b7c672f50cf220ee0e936e4174fe
xhugo92 <hugofrn1992@hotmail.com>
2020-08-27 19:17:20 -0300

    Falha no aceite da conexão consertado

--- Passivo_Lab1.py
+++ Passivo_Lab1.py
@@ -6,7 +6,6 @@ PORTA = 7000
 sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST,PORTA))
 sock.listen(1)
-sock.accept()
 nsock, endereco = sock.accept()
 while True:
     msg= nsock.recv(1024)
